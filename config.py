b13818eac157b5c91901eadacc37766a7d734c6e
manish8000 <147980408+manish8000@users.noreply.github.com>
2026-08-15 10:23:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+# Telegram API Credentials (my.telegram.org से मिलते हैं)
+API_ID = int(os.environ.get("API_ID", "123456"))  
+API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+
+# Bot Token & Mongo
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
+MONGO_URI = os.environ.get("MONGO_URI", "YOUR_MONGO_URI")
+
+# Admin Settings
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "123456789"))
